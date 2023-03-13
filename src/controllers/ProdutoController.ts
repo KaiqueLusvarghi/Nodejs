510f9f189d91f14f2e1d0447b8ff0ee0c117626d
KaiqueLusvarghi <kaiqueolusvarghi@gmail.com>
2023-03-13 13:37:47 -0300

    alteração no Produto Controller

--- src/controllers/ProdutoController.ts
+++ src/controllers/ProdutoController.ts
@@ -2,13 +2,21 @@ import { Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
 
 class ProdutoController {
-    async index(req: Request, res: Response) {
-        const prisma = new PrismaClient();
-        const produtos = await prisma.produto.findMany();//Recuperar produtos  
-        
-        res.status(200).json(produtos);
-
-    } async show(req: Request, res: Response) {
+     async index(req:Request,res:Response){        
+        const prisma = new PrismaClient();        
+        const produtos = await prisma.produto.findMany({
+            orderBy:{nome:'asc'},                
+            select:{
+                nome:true,  // seleciona as propriedade desejadas de Produto
+                preco:true,
+                Categoria:{
+                    select:{nome:true}  // traz do model relacionado Categoria apenas o nome
+                }
+            }
+        }); // recupera todos os produto        
+        res.status(200).json(produtos);    
+    }
+    async show(req: Request, res: Response) {
         const prisma = new PrismaClient();
         const produto = await prisma.produto.findUnique(// busca produto conforme where            
 
